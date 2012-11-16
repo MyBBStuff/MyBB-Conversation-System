ce9a1abe49281aa4101901a31ddb0ae01322d02b
Euan <euantorano@gmail.com>
2012-11-16 19:03:34 +0000

    Adding activation function.

--- inc/languages/english/admin/mybbconversations.lang.php
+++ inc/languages/english/admin/mybbconversations.lang.php
@@ -14,3 +14,11 @@
 
 $l['mybbconversations_title'] = 'MyBB Conversation System';
 $l['mybbconversations_description'] = 'A replacement for MyBB\'s core Private Message functionality based on IPB\'s Conversation feature.';
+
+$l['mybbconversations_pluginlibrary_missing'] = 'This plugin requires <a href="http://mods.mybb.com/view/pluginlibrary">PluginLibrary</a> to function.';
+
+$l['setting_group_mybbconversations']         = 'MyBB Conversation System Settings';
+$l['setting_group_mybbconversations_desc']    = 'Settings for the MyBB conversation system.';
+
+$l['setting_mybbconversations_enabled']       = 'Enable the conversation system?';
+$l['setting_mybbconversations_enabled_desc']  = 'You can use this switch to globally enable or disable the Conversation System.';
--- inc/plugins/mybbconversations.php
+++ inc/plugins/mybbconversations.php
@@ -66,17 +66,6 @@ function mybbconversations_install()
         admin_redirect('index.php?module=config-plugins');
     }
 
-    $plugin_info = myalerts_info();
-    $euantor_plugins = $cache->read('euantor_plugins');
-    if (empty($euantor_plugins) OR !is_array($euantor_plugins)) {
-        $euantor_plugins = array();
-    }
-    $euantor_plugins['mybbconversations'] = array(
-        'title'   =>  'MyBB Conversation System',
-        'version' =>  $plugin_info['version'],
-    );
-    $cache->update('euantor_plugins', $euantor_plugins);
-
     $collation = $db->build_create_table_collation();
 
     if (!$db->table_exists('conversations')) {
@@ -128,16 +117,13 @@ function mybbconversations_uninstall()
     global $db, $cache;
 
     if (!file_exists(PLUGINLIBRARY)) {
-        flash_message('The selected plugin could not be uninstalled because <a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a> is missing.', 'error');
+        flash_message($lang->mybbconversations_pluginlibrary_missing, 'error');
         admin_redirect('index.php?module=config-plugins');
     }
 
     $PL or include_once PLUGINLIBRARY;
 
-    if ((int) $PL->version < 9) {
-        flash_message('This plugin requires PluginLibrary 9 or newer', 'error');
-        admin_redirect('index.php?module=config-plugins');
-    }
+    $PL->settings_delete('mybbconversations', true);
 
     if ($db->table_exists('conversations')) {
         $db->drop_table('conversations');
@@ -152,4 +138,48 @@ function mybbconversations_uninstall()
         unset($euantor_plugins['mybbconversations']);
     }
     $cache->update('euantor_plugins', $euantor_plugins);
+}
+
+/**
+ * Activation function.
+ * @return null
+ */
+function mybbconversations_activate()
+{
+    global $lang, $PL, $cache;
+
+    if (!$lang->mybbconversations) {
+        $lang->load('mybbconversations');
+    }
+
+    if (!file_exists(PLUGINLIBRARY)) {
+        flash_message($lang->mybbconversations_pluginlibrary_missing, 'error');
+        admin_redirect('index.php?module=config-plugins');
+    }
+
+    $PL or require_once PLUGINLIBRARY;
+
+    $plugin_info = myalerts_info();
+    $euantor_plugins = $cache->read('euantor_plugins');
+    if (empty($euantor_plugins) OR !is_array($euantor_plugins)) {
+        $euantor_plugins = array();
+    }
+    $euantor_plugins['mybbconversations'] = array(
+        'title'   =>  'MyBB Conversation System',
+        'version' =>  $plugin_info['version'],
+    );
+    $cache->update('euantor_plugins', $euantor_plugins);
+
+    $PL->settings(
+        'mybbconversations',
+        $lang->setting_group_mybbconversations,
+        $lang->setting_group_mybbconversations_desc,
+        array(
+            'enabled'   =>  array(
+                'title'         =>  $lang->setting_mybbconversations_enabled,
+                'description'   =>  $lang->setting_mybbconversations_enabled_desc,
+                'value'         =>  '1',
+            ),
+        )
+    );
 }
